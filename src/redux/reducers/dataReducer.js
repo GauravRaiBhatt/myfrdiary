7b6a94f3676929bf609ee87f705a62b196318547
Gaurav Rai Bhatt <gaurav.rai.bhatt@gmail.com>
2021-06-13 10:15:44 +0530

    fixed local variable redecleration

--- src/redux/reducers/dataReducer.js
+++ src/redux/reducers/dataReducer.js
@@ -1,11 +1,12 @@
 import actionTypes from "../actionTypes";
 
 const INITIAL_STATE = { recepientData: [] };
+let newrecepientData;
 
 const dataReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case actionTypes.ADD_RECEPIENT:
-      let newrecepientData = [...state.recepientData];
+       newrecepientData = [...state.recepientData];
       action.payload.length
         ? newrecepientData.push(...action.payload)
         : newrecepientData.push(action.payload);
@@ -14,7 +15,7 @@ const dataReducer = (state = INITIAL_STATE, action) => {
       };
 
     case actionTypes.REMOVE_ONERECEPIENT:
-      let newrecepientData = [
+      newrecepientData = [
         state.recepientData.filter((recepient) =>
           recepient.recepientId === action.payload ? "" : recepient
         ),
